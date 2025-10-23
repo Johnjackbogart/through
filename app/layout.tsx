24e25cf98de0133f76b359f8007f9275599e47d8
John Bogart <johnjackbogart@gmail.com>
2025-10-23 12:35:21 -0400

    this is nice and dark mode works

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,25 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
-import { ThemeProvider } from "@/components/theme-provider"
-import "./globals.css"
-import { IBM_Plex_Mono } from "next/font/google"
+import type React from "react";
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
+import { ThemeProvider } from "@/components/theme-provider";
+import "./globals.css";
+import { IBM_Plex_Mono } from "next/font/google";
 
-const _geist = Geist({ subsets: ["latin"] })
-const _geistMono = Geist_Mono({ subsets: ["latin"] })
 const ibmPlexMono = IBM_Plex_Mono({
   subsets: ["latin"],
   weight: "100",
-})
+});
 
 export const metadata: Metadata = {
   title: "v0 App",
   description: "Created with v0",
   generator: "v0.app",
-}
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
@@ -33,5 +30,5 @@ export default function RootLayout({
         <Analytics />
       </body>
     </html>
-  )
+  );
 }
